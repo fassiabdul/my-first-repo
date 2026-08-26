3ce0ff260143275667fad90d821836614950c43a
fassiabdul <abdulfassi786@gmail.com>
2026-08-26 17:01:22 +0530

    Add Sample class with main method

---
+++ happy.java
@@ -0,0 +1,17 @@
+public class Sample {
+
+    public static void main(String[] args) {
+
+        String name = "John";
+        int age = 25;
+
+        System.out.println("Hello, " + name);
+        System.out.println("Age: " + age);
+
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a minor.");
+        }
+    }
+}
